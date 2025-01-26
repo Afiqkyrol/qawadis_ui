093e087a0ee4731cc84d7a2861a9a3f619c28df6
Afiqkyrol <94352680+Afiqkyrol@users.noreply.github.com>
2025-01-26 22:56:03 +0800

    progress

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import InnerLayout from "../lib/component/layout/defaultLayout";
+import InnerLayout from "../lib/component/layout/innerLayout";
 import { redirect } from "next/navigation";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
