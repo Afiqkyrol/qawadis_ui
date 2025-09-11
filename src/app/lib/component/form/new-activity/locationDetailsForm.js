f50ab665f7d7850fe611d91c7b70dc357df1e27e
Afiqkyrol <94352680+Afiqkyrol@users.noreply.github.com>
2025-09-11 08:37:14 +0800

    Update locationDetailsForm.js

--- src/app/lib/component/form/new-activity/locationDetailsForm.js
+++ src/app/lib/component/form/new-activity/locationDetailsForm.js
@@ -31,89 +31,87 @@ export default function LocationDetailsForm({ form, setForm }) {
   }
 
   return (
-    <>
-      <Grid
-        style={{ marginTop: "2rem", marginBottom: "2rem" }}
-        gutter="sm"
-        justify="center"
+    <Grid
+      style={{ marginTop: "2rem", marginBottom: "2rem" }}
+      gutter="sm"
+      justify="center"
+    >
+      <Grid.Col
+        style={{
+          justifyItems: "stretch",
+          paddingTop: "0",
+          paddingBottom: "0",
+        }}
+        span={{ sm: 12, base: 12, md: 6, lg: 6 }}
       >
-        <Grid.Col
+        <SmartTextInput
+          controlName="venue"
+          label="Venue"
+          placeholder="Enter venue"
+          value={form.venue}
+          required={true}
+          error={errors.venue}
+          onChange={inputHandler}
+          valueValidator={() => validateField("venue", form.venue)}
+        />
+      </Grid.Col>
+      <Grid.Col
+        style={{
+          justifyItems: "stretch",
+          paddingTop: "0",
+          paddingBottom: "0",
+        }}
+        span={{ sm: 12, base: 12, md: 6, lg: 6 }}
+      >
+        <SmartTextInput
+          controlName="address"
+          label="Address"
+          placeholder="Enter address"
+          required={true}
+          value={form.address}
+          error={errors.address}
+          onChange={inputHandler}
+          valueValidator={() => validateField("address", form.address)}
+        />
+      </Grid.Col>
+      <Grid.Col
+        style={{
+          justifyItems: "stretch",
+          paddingTop: "0",
+          paddingBottom: "0",
+        }}
+        span={{ sm: 6, base: 12, md: 9, lg: 10 }}
+      >
+        <SmartTextInput
+          controlName="mapLink"
+          label="Google Maps Link"
+          placeholder="Enter link"
+          value={form.mapLink}
+          error={errors.mapLink}
+          onChange={inputHandler}
+          valueValidator={() => validateField("mapLink", form.mapLink)}
+        />
+      </Grid.Col>
+      <Grid.Col
+        style={{
+          justifyItems: "stretch",
+          alignContent: "center",
+          paddingTop: "0",
+          paddingBottom: "0",
+        }}
+        span={{ sm: 6, base: 12, md: 3, lg: 2 }}
+      >
+        <SmartButton
+          text="Check Map"
+          variant="outline"
           style={{
-            justifyItems: "stretch",
-            paddingTop: "0",
-            paddingBottom: "0",
+            marginTop: "6px",
           }}
-          span={{ sm: 12, base: 12, md: 6, lg: 6 }}
-        >
-          <SmartTextInput
-            controlName="venue"
-            label="Venue"
-            placeholder="Enter venue"
-            value={form.venue}
-            required={true}
-            error={errors.venue}
-            onChange={inputHandler}
-            valueValidator={() => validateField("venue", form.venue)}
-          />
-        </Grid.Col>
-        <Grid.Col
-          style={{
-            justifyItems: "stretch",
-            paddingTop: "0",
-            paddingBottom: "0",
-          }}
-          span={{ sm: 12, base: 12, md: 6, lg: 6 }}
-        >
-          <SmartTextInput
-            controlName="address"
-            label="Address"
-            placeholder="Enter address"
-            required={true}
-            value={form.address}
-            error={errors.address}
-            onChange={inputHandler}
-            valueValidator={() => validateField("address", form.address)}
-          />
-        </Grid.Col>
-        <Grid.Col
-          style={{
-            justifyItems: "stretch",
-            paddingTop: "0",
-            paddingBottom: "0",
-          }}
-          span={{ sm: 6, base: 12, md: 9, lg: 10 }}
-        >
-          <SmartTextInput
-            controlName="mapLink"
-            label="Google Maps Link"
-            placeholder="Enter link"
-            value={form.mapLink}
-            error={errors.mapLink}
-            onChange={inputHandler}
-            valueValidator={() => validateField("mapLink", form.mapLink)}
-          />
-        </Grid.Col>
-        <Grid.Col
-          style={{
-            justifyItems: "stretch",
-            alignContent: "center",
-            paddingTop: "0",
-            paddingBottom: "0",
-          }}
-          span={{ sm: 6, base: 12, md: 3, lg: 2 }}
-        >
-          <SmartButton
-            text="Check Map"
-            variant="outline"
-            style={{
-              marginTop: "6px",
-            }}
-            icon={<IconCheck size={14} />}
-            // loading={loadingSearch}
-            submitHandler={async () => {}}
-          />
-        </Grid.Col>
-      </Grid>
-    </>
+          icon={<IconCheck size={14} />}
+          // loading={loadingSearch}
+          submitHandler={async () => {}}
+        />
+      </Grid.Col>
+    </Grid>
   );
 }
